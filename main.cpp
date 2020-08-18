2cf8831130ff48962399dfda3beeb29cb0e908fd
YdrMaster <ydrml@hotmail.com>
2020-08-18 18:18:52 +0800

    refactor: 算法仿真分离到函数，降低耦合，方便测试多种算法

--- main.cpp
+++ main.cpp
@@ -11,10 +11,14 @@
 
 using namespace mechdancer;
 
+constexpr static auto PATH = "../data"; // 数据保存路径
 template<class t> requires Number<t>
 constexpr auto sound_speed(t temperature) {
     return 20.048 * std::sqrt(temperature + 273.15);
 }
+
+template<class t, class u> requires RealSignal<t> && RealSignal<u>
+void simulation(t, u);
 
 int main() {
     // region 准备环境
@@ -25,26 +29,20 @@ int main() {
     std::filesystem::create_directory("../data");
     // endregion
     // region 参数
-    constexpr static auto PATH = "../data";  // 数据保存路径
     constexpr static auto MAIN_FS = 1_MHz;   // 仿真采样率（取决于测量脉冲响应的采样率）
     constexpr static auto DISTANCE = 3;      // 实际距离（米）
     constexpr static auto TEMPERATURE = 20;  // 气温（℃）
-    constexpr static auto FRAME_SIZE = 1024; // 帧长度
     // endregion
     // region 信源信道仿真
-    // 收发系统
-    auto transceiver0 = load("../2048_1M_0.txt", MAIN_FS, 0s);
-    auto transceiver1 = load("../2048_1M_1.txt", MAIN_FS, 0s);
-    // 激励信号（加噪）
+    auto transceiver = load("../2048_1M_0.txt", MAIN_FS, 0s);
     auto excitation = sample(1'000, chirp(39_kHz, 61_kHz, 1ms), MAIN_FS, 0s);
-    // 参考接收
-    add_noise_measured(transceiver0, -3_db);
-    auto reference0 = convolution(transceiver0, excitation); // 构造接收信号
-    auto reference1 = convolution(transceiver1, excitation); // 构造参考信号
+    auto excitation_noise = excitation;
+    add_noise_measured(excitation_noise, -3_db);
+    auto reference = convolution(transceiver, excitation_noise); // 构造接收信号
     // 加噪
     auto DELAY = static_cast<size_t>(std::lround(DISTANCE * MAIN_FS.cast_to<Hz_t>().value / sound_speed(TEMPERATURE)));
-    auto received = real_signal_of(DELAY + 3 * reference0.values.size(), reference0.sampling_frequency, 0s);
-    std::copy(reference0.values.begin(), reference0.values.end(), received.values.begin() + DELAY);
+    auto received = real_signal_of(DELAY + 3 * reference.values.size(), reference.sampling_frequency, 0s);
+    std::copy(reference.values.begin(), reference.values.end(), received.values.begin() + DELAY);
     add_noise(received, sigma_noise(received, -12_db));
     // endregion
     // region 接收机仿真
@@ -56,37 +54,41 @@ int main() {
     std::transform(sampling_float.values.begin(), sampling_float.values.end(), sampling.values.begin(),
                    [](auto x) { return static_cast<sample_t>(x / 15 + 1600); });
     SAVE_SIGNAL_AUTO({ PATH }, sampling)
+    std::cout << "延迟点数 = " << DELAY * .6 << std::endl;
     { // 测试互相关
-        auto reff = resample(reference0, 600_kHz, 64);
-        auto corr = correlation<correlation_mode::noise_reduction>(reff, sampling_float);
-        std::cout << "延迟点数 = " << DELAY * .6 << std::endl;
-        SAVE_SIGNAL_AUTO({ PATH }, reff)
-        SAVE_SIGNAL_AUTO({ PATH }, corr)
+        auto test = correlation(resample(reference, 600_kHz, 64), sampling_float);
+        SAVE_SIGNAL_AUTO({ PATH }, test)
     }
-    // 重叠分帧，模拟内存不足的嵌入式系统
-    auto frames = std::vector<decltype(sampling)>();
-    {
-        using _time_t = typename decltype(sampling)::time_t;
-        auto fs = sampling.sampling_frequency;
-        for (auto i = 0; i < sampling.values.size(); i += FRAME_SIZE) {
-            frames.push_back({std::vector<sample_t>(FRAME_SIZE), fs, fs.duration_of<_time_t>(i)});
-            if (i + FRAME_SIZE < sampling.values.size())
-                std::copy_n(sampling.values.begin() + i, FRAME_SIZE, frames.back().values.begin());
-            else
-                std::copy(sampling.values.begin() + i, sampling.values.end(), frames.back().values.begin());
-        }
-        // 分帧保存到文件
-        std::ofstream file("../data/frames.txt");
-        for (size_t i = 0; i < FRAME_SIZE; ++i) {
-            for (auto const &frame : frames)
-                file << frame.values[i] << '\t';
-            file << std::endl;
-        }
+    // endregion
+    simulation(convolution(transceiver, excitation), sampling);
+    return 0;
+}
+
+template<class t, class u> requires RealSignal<t> && RealSignal<u>
+void simulation(t reference, u sampling) {
+    constexpr static auto FRAME_SIZE = 1024; // 帧长度
+    // region 重叠分帧，模拟内存不足的嵌入式系统
+    auto frames = std::vector<u>();
+    using Tt = typename u::time_t;
+    auto fs = sampling.sampling_frequency;
+    for (auto i = 0; i < sampling.values.size(); i += FRAME_SIZE) {
+        frames.push_back({decltype(sampling.values)(FRAME_SIZE), fs, fs.template duration_of<Tt>(i)});
+        if (i + FRAME_SIZE < sampling.values.size())
+            std::copy_n(sampling.values.begin() + i, FRAME_SIZE, frames.back().values.begin());
+        else
+            std::copy(sampling.values.begin() + i, sampling.values.end(), frames.back().values.begin());
+    }
+    // 分帧保存到文件
+    std::ofstream file("../data/frames.txt");
+    for (size_t i = 0; i < FRAME_SIZE; ++i) {
+        for (auto const &frame : frames)
+            file << frame.values[i] << '\t';
+        file << std::endl;
     }
     // endregion
     // region 算法仿真
-    auto buffer = real_signal_of<int>(768, 150_kHz, 0s);
-    auto reference150kHz = resample(reference1, 150_kHz, 4);
+    auto buffer = real_signal_of<int>(768, 150_kHz, floating_seconds(0));
+    auto reference150kHz = resample(reference, 150_kHz, 4);
     reference150kHz.values.erase(reference150kHz.values.begin() + 257, reference150kHz.values.end());
     auto result = std::vector<common_type<decltype(buffer), decltype(reference150kHz)>>();
     for (auto i = 0; i < frames.size(); ++i) {
@@ -111,7 +113,7 @@ int main() {
     }
     // endregion
     // region 生成结论
-    auto sum = real_signal_of(512 * (result.size() + 1), 150_kHz, 0s);
+    auto sum = real_signal_of(512 * (result.size() + 1), 150_kHz, floating_seconds(0));
     for (auto i = 0; i < result.size(); ++i)
         for (auto j = 0; j < result[i].values.size(); ++j)
             sum.values[i * 512 + j] += result[i].values[j];
@@ -122,5 +124,4 @@ int main() {
             max_i = i;
     std::cout << "估计点数 = " << (max_i - 256) * 4 << std::endl;
     // endregion
-    return 0;
 }
